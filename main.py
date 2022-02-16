edb0d9522621203a3fc21a624efc6255c1584d1c
Jim <chengxuyuan9527@gmail.com>
2022-02-17 00:17:54 +1100

    updates
    updates

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+
+stdout_main = sys.stdout
+
 states1 = ["Victoria", "New South Wales", "Queensland", "Northern Territory"]
 states2 = ["Victoria", "New South Wales", "Queensland", "Tasmania", "South Australia"]
 address1 = {
@@ -15,12 +19,12 @@ address2 = {
 
 
 class Vehicle:
-    def __init__(self, rego, make, model, current_odometer):
+    def __init__(self, rego, make, model, current_odometer, driver):
         self.rego = rego
         self.make = make
         self.model = model
         self.current_odometer = current_odometer
-        self.driver = None
+        self.driver = driver
 
     def display_odometer(self):
         print(f"The vehicle current odometer is {self.current_odometer} ")
@@ -29,61 +33,51 @@ class Vehicle:
         self.current_odometer += km
         if self.current_odometer < 0:
             print("The odometer is not valid ")
-        else:
-            print(f"The vehicle current odometer is {self.current_odometer} ")
+
+    def vehicle_info(self):
+        print(f"""
+            Vehicle registration number {self.rego} is a {self.make}. Model is {self.model}.Odometer {self.current_odometer} km. 
+        """)
 
 
 class Car(Vehicle):
     def __init__(self, rego, make, model, current_odometer, body_type, color, upholstery, num_of_doors, driver):
-        super().__init__(rego, make, model, current_odometer)
+        super().__init__(rego, make, model, current_odometer, driver)
         self.body_type = body_type
         self.color = color
         self.upholstery = upholstery
         self.number_of_doors = num_of_doors
-        self.driver = driver
 
     def display_color(self):
-        print(f"The color of the car is {self.color}")
+        print(f"The color of {self.make}-{self.model} is {self.color}")
 
     def change_color(self, c_color):
         self.color = c_color
 
-    def car_general_info(self):
-        print(
-            f"Vehicle registration number {self.rego} is a {self.make}.Model is {self.model}.Odometer {self.current_odometer} km")
-
-    def car_spec_info(self):
-        print(f"The car details are: {self.body_type}, {self.color}, {self.upholstery}, {self.number_of_doors}")
-
-    def car_with_driver(self):
-        self.car_general_info()
-        print(
-            f"The driver of the vehicle is {self.driver.f_name} {self.driver.f_name}\nDriver License number: {self.driver.licence_number}")
-        print("Additional details:")
-        self.car_spec_info()
+    def car_info(self):
+        print(f"""
+            Vehicle registration number {self.rego} is a {self.make}. Model is {self.model}.Odometer {self.current_odometer} km.
+            The driver of the vehicle is {self.driver.f_name} {self.driver.l_name}. Driver License number: {self.driver.licence_number}
+            Additional details:
+            The car details are: body type {self.body_type}, color {self.color}, {self.upholstery} interior, {self.number_of_doors} doors.
+        """)
 
 
 class Truck(Vehicle):
     def __init__(self, rego, make, model, current_odometer, max_load_cap, num_of_axles, num_of_wheels, driver):
-        super().__init__(rego, make, model, current_odometer)
+        super().__init__(rego, make, model, current_odometer, driver)
         self.max_load_cap = max_load_cap
         self.num_of_axles = num_of_axles
         self.num_of_wheels = num_of_wheels
         self.driver = driver
 
-    def truck_general_info(self):
-        print(
-            f"Vehicle registration number {self.rego} is a {self.make}.Model is {self.model}.Odometer {self.current_odometer} km")
-
-    def truck_spec_info(self):
-        print(f"The truck details are: {self.max_load_cap}, {self.num_of_axles}, {self.num_of_wheels}")
-
-    def truck_with_driver(self):
-        self.truck_general_info()
-        print(
-            f"The driver of the vehicle is {self.driver.f_name} {self.driver.f_name}\nDriver License {self.driver.licence_number}")
-        print("Additional details:")
-        self.truck_spec_info()
+    def truck_info(self):
+        print(f"""
+               Vehicle registration number {self.rego} is a {self.make}. Model is {self.model}.Odometer {self.current_odometer} km.
+               The driver of the vehicle is {self.driver.f_name} {self.driver.l_name}. Driver License number: {self.driver.licence_number}
+               Additional details:
+               The truck details are: max_load_cap {self.max_load_cap}, {self.num_of_axles} axles, {self.num_of_wheels} wheels.
+           """)
 
 
 class Driver:
@@ -100,123 +94,126 @@ class Driver:
         self.states_of_drive = states_of_drive
         self.current_demerit_points = current_demerit_points
 
-    def display_all_info(self):
-        print(f"The driver {self.f_name} {self.l_name}, has a driver licence number : {self.licence_number}Contact "
-              f"phone number is : {self.mobile}\nDriver address is : ")
-        self.display_address()
-
-    def display_address(self):
-        for (k, v) in self.address.items():
-            print(f"{k} : {v}")
-
-    def display_states_of_drive(self):
-        print("The driver is licenced to drive in the following states:")
-        for state in self.states_of_drive:
-            print(f"{state}:")
-
     def display_current_demerit_points(self):
-        print(f"The driver {self.f_name} {self.l_name} 's current demerit points is {self.current_demerit_points}")
+        print(f"The driver {self.f_name} {self.l_name}'s current demerit points is {self.current_demerit_points}.")
 
     def decrease_demerit(self, number):
         self.current_demerit_points -= number
         if self.current_demerit_points < Driver.min_demerit_points:
             print(f"Demerit points should not be allowed to fall below {Driver.min_demerit_points}")
 
-        elif self.current_demerit_points > Driver.demerit_warning:
+        elif self.current_demerit_points >= Driver.demerit_warning:
             print("License suspension is imminent")
-        else:
-            print(f"The driver {self.f_name} {self.l_name} 's current demerit points is {self.current_demerit_points}")
 
     def increase_demerit(self, number):
         self.current_demerit_points += number
         if self.current_demerit_points > Driver.max_demerit_points:
             print(f"Demerit points should not be allowed to increase beyond {Driver.max_demerit_points}")
-        elif self.current_demerit_points > Driver.demerit_warning:
+        elif self.current_demerit_points >= Driver.demerit_warning:
             print("License suspension is imminent")
         else:
             print(f"The driver {self.f_name} {self.l_name} 's current demerit points is {self.current_demerit_points}")
 
+    def driver_info(self):
+        print(
+            f"The driver {self.f_name} {self.l_name}, has a driver licence number : {self.licence_number}\nContact phone number is : {self.mobile}\nDriver address is :")
+        for (k, v) in self.address.items():
+            print(f"{k} : {v}")
+        print("The driver is licenced to drive in the following states:")
+        for state in self.states_of_drive:
+            print(f"{state}", end=" :  ")
+        print()
+
+    def write_driver_info(self):
+        sys.stdout = open("DriverInfo.txt", "w")
+        self.driver_info()
+        sys.stdout.close()
+        sys.stdout = stdout_main
+
+    @staticmethod
+    def read_driver_info():
+        with open("DriverInfo.txt", "r") as file:
+            contents = file.read()
+            print(contents)
+
 
 def main():
+    input("Press Enter to continue")
     driver1 = Driver(3313377, "Gladys", "Berejkilan", "0414566999", address1, states1, 8)
-    driver1.display_all_info()
-    input("Press Enter to continue")
-    driver1.display_address()
-    input("Press Enter to continue")
-    driver1.display_states_of_drive()
-    input("Press Enter to continue")
     driver1.display_current_demerit_points()
     input("Press Enter to continue")
     driver1.decrease_demerit(2)
+    driver1.display_current_demerit_points()
     input("Press Enter to continue")
     driver1.increase_demerit(3)
     input("Press Enter to continue")
+    driver1.display_current_demerit_points()
+    input("Press Enter to continue")
+    driver1.driver_info()
+    input("Press Enter to continue")
+    driver1.write_driver_info()
+    driver1.read_driver_info()
 
     driver2 = Driver(9877345, "Boris", "Johnson", "0414123456", address2, states2, 3)
-    driver2.display_all_info()
-    input("Press Enter to continue")
-    driver2.display_address()
-    input("Press Enter to continue")
-    driver2.display_states_of_drive()
-    input("Press Enter to continue")
     driver2.display_current_demerit_points()
     input("Press Enter to continue")
     driver2.decrease_demerit(2)
+    driver2.display_current_demerit_points()
     input("Press Enter to continue")
-    driver2.increase_demerit(3)
+    driver2.increase_demerit(13)
+    driver2.display_current_demerit_points()
+    input("Press Enter to continue")
+    driver2.driver_info()
+    driver2.write_driver_info()
+    driver2.read_driver_info()
 
     car1 = Car("BBJ702", "Mazda", "CX3", 10000, "Hatch", "Blue", "Leather", 5, driver1)
     input("Press Enter to continue")
+    car1.vehicle_info()
+    input("Press Enter to continue")
     car1.display_color()
+    input("Press Enter to continue")
     car1.change_color("White")
-    input("Press Enter to continue")
     car1.display_color()
     input("Press Enter to continue")
-    car1.car_general_info()
+    car1.display_odometer()
+    car1.update_odometer(29000)
+    car1.display_odometer()
     input("Press Enter to continue")
-    car1.car_spec_info()
-    input("Press Enter to continue")
-    car1.car_with_driver()
+    car1.car_info()
 
     car2 = Car("OY0400", "Ford", "Festiva", 39785, "Sedan", "Green", "Fabric", 4, driver2)
     input("Press Enter to continue")
-    car2.display_color()
-    car2.change_color("White")
+    car2.vehicle_info()
     input("Press Enter to continue")
     car2.display_color()
     input("Press Enter to continue")
-    car2.car_general_info()
+    car2.change_color("Black")
+    car2.display_color()
+    car2.display_odometer()
+    car2.update_odometer(10000)
+    car2.display_odometer()
     input("Press Enter to continue")
-    car2.car_spec_info()
-    input("Press Enter to continue")
-    car2.car_with_driver()
+    car2.car_info()
 
     truck1 = Truck("XJBJ882", "Kenworth", "BigMother5000", 150000, "40 tonnes", 5, 18, driver1)
     input("Press Enter to continue")
-    truck1.truck_general_info()
+    truck1.vehicle_info()
     input("Press Enter to continue")
-    truck1.truck_spec_info()
-    input("Press Enter to continue")
-    truck1.truck_with_driver()
-    input("Press Enter to continue")
-
+    truck1.display_odometer()
+    truck1.update_odometer(15000)
     truck1.display_odometer()
     input("Press Enter to continue")
-    truck1.update_odometer(20000)
+    truck1.truck_info()
 
     truck2 = Truck("ARC542", "Hyundai", "iLoad", 76520, "2 tonnes", 2, 4, driver2)
     input("Press Enter to continue")
-    truck2.truck_general_info()
-    input("Press Enter to continue")
-    truck2.truck_spec_info()
-    input("Press Enter to continue")
-    truck2.truck_with_driver()
-    input("Press Enter to continue")
-
+    truck2.vehicle_info()
+    truck2.display_odometer()
+    truck2.update_odometer(-29000000)
     truck2.display_odometer()
     input("Press Enter to continue")
-    truck2.update_odometer(10000)
-    input("Press Enter to continue")
+    truck2.truck_info()
 
 
 if __name__ == "__main__":
